6a13999db028bc15761bfd3fef7ee9bf08551f4d
jhfranco2 <jhoanmateofranco@gmail.com>
2023-10-11 12:57:41 -0500

    add: se añade metodo para editar un propietario y sus vehiculos

--- src/main/java/com/detektor/inventarioback/controladores/PropietarioControllador.java
+++ src/main/java/com/detektor/inventarioback/controladores/PropietarioControllador.java
@@ -3,15 +3,19 @@ package com.detektor.inventarioback.controladores;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,9 +30,21 @@ public class PropietarioControllador {
     @Autowired
     private PropietarioServiceImpl propietarioServiceImpl;
 
-    @GetMapping("/propietarios")
-    public List<Propietario> getAll() {
-        return propietarioServiceImpl.getAll();
+    @GetMapping("propietario/{id}")
+    public ResponseEntity<?> getPropietario(@PathVariable Long id) {
+        Map<String, Object> response;
+        try {
+            // Busca el propietario con el ID especificado
+            Propietario propietarioEncontrado = propietarioServiceImpl.findById(id).get();
+
+            // Crea la respuesta
+            response = new HashMap<>();
+            response.put("propietario", propietarioEncontrado);
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
     }
 
     @PostMapping("save-propietario")
@@ -46,4 +62,41 @@ public class PropietarioControllador {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
         }
     }
+
+    @PutMapping("edit-propietario/{id}")
+    public ResponseEntity<?> editPropietario(@PathVariable Long id, @RequestPart Propietario propietario) {
+        Map<String, String> message;
+        try {
+            // Busca el propietario existente con el ID especificado
+            Optional<Propietario> propietarioOptional = propietarioServiceImpl.findById(id);
+
+            if (propietarioOptional.isPresent()) {
+                Propietario propietarioEncontrado = propietarioOptional.get();
+
+                // Actualiza los campos del propietario existente con los nuevos valores
+                propietarioEncontrado.setNombre(propietario.getNombre());
+                propietarioEncontrado.setApellido(propietario.getApellido());
+                propietarioEncontrado.setIdentificacion(propietario.getIdentificacion());
+                propietarioEncontrado.setFechaNachimiento(propietario.getFechaNachimiento());
+                propietarioEncontrado.setVehiculo(propietario.getVehiculo());
+
+                // Guarda el propietario actualizado
+                propietarioServiceImpl.save(propietarioEncontrado);
+
+                message = new HashMap<>();
+                message.put("message", "Propietario editado exitosamente");
+                return ResponseEntity.status(HttpStatus.OK).body(message);
+            } else {
+                message = new HashMap<>();
+                message.put("message", "No se pudo encontrar el propietario con el ID especificado");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+            }
+        } catch (Exception e) {
+            message = new HashMap<>();
+            message.put("message", "No se pudo editar el propietario");
+            message.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+        }
+    }
+
 }
